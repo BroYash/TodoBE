53f53417e318cd9fdf7af7ae85e16d33fe78dca2
Yashjeet <yashjeetbajwa8@gmail.com>
2022-10-14 20:09:29 +1300

    Add datacontext

---
+++ src/Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class DataContext : IdentityDbContext<ApplicationUser>
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+}
